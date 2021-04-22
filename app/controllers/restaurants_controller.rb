0e955c438f50857b4752a3851df06b5e8d0fb09b
Eduardo Scovino <eduardo.scovino@gmail.com>
2021-04-21 23:50:07 -0300

    delete + refactoring

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,12 @@
 class RestaurantsController < ApplicationController
 
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -20,12 +21,16 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
 
     redirect_to restaurant_path(@restaurant)
   end
@@ -34,5 +39,9 @@ class RestaurantsController < ApplicationController
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
